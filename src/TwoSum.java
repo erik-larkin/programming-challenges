1ac715ba05b30cd23f9cd3c3f2324fca22cd1b52
erik-larkin <erik.larkin789@gmail.com>
2023-08-27 17:53:19 +0100

    improved two sum

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -11,18 +11,17 @@ public class TwoSum {
 
 	public int[] twoSum(int[] nums, int target) {
 
-		Map<Integer, Integer> numberIndices = new HashMap<Integer, Integer>(nums.length);
-
-		for (int i = 0; i < nums.length; i++) {
-			numberIndices.put(nums[i], i);
-		}
+		Map<Integer, Integer> numberIndices = new HashMap<Integer, Integer>();
 
 		for (int i = 0; i < nums.length; i++) {
 			int remainder = target - nums[i];
 			Integer j = numberIndices.get(remainder);
+			
 			if (j != null && j != i) {
 				return new int[]{i, j};
 			}
+
+			numberIndices.put(nums[i], i);
 		}
 
 		return null;
