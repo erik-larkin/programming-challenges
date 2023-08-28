e0bd20e852b618b0fbf912288ea69c29cb07a1ee
erik-larkin <erik.larkin789@gmail.com>
2023-08-28 17:17:25 +0100

    product of array except self

---
+++ src/ProductOfArrayExceptSelf.java
@@ -0,0 +1,21 @@
+public class ProductOfArrayExceptSelf 
+{
+	public int[] productExceptSelf(int[] nums) 
+	{
+		int[] result = new int[nums.length];
+		result[0] = 1;
+
+		for (int i = 1; i < result.length; i++)
+			result[i] = result[i-1] * nums[i-1];
+		
+		int suffixProduct = nums[nums.length - 1];
+
+		for (int i = result.length - 2; i >= 0; i--)
+		{
+			result[i] *= suffixProduct;
+			suffixProduct *= nums[i];
+		}
+
+		return result;
+	}
+}
