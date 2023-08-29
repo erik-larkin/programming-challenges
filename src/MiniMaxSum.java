510711c97684b2fe1f94dc3e6d8797a80ea0fae8
erik-larkin <erik.larkin789@gmail.com>
2023-08-29 11:36:14 +0100

    mini max sum

---
+++ src/MiniMaxSum.java
@@ -0,0 +1,25 @@
+/*
+ *  https://www.hackerrank.com/challenges/one-week-preparation-kit-mini-max-sum/problem
+ *  August 27th 2023
+ *  By Erik Larkin
+ */
+
+import java.util.List;
+
+public class MiniMaxSum {
+
+	public static void miniMaxSum(List<Integer> arr) {
+
+		int min = arr.get(0);
+		int max = arr.get(0);
+		long sum = 0;
+
+		for (int number : arr) {
+			sum += number;
+			min = Math.min(min, number);
+			max = Math.max(max, number);
+		}
+
+		System.out.printf("%d %d", sum - max, sum - min);
+    }
+}
