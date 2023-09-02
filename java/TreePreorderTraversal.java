d788fd178d5f155041599a28c8119a7318ff42ec
erik-larkin <erik.larkin789@gmail.com>
2023-09-02 15:41:40 +0100

    tree preorder traversal

---
+++ java/TreePreorderTraversal.java
@@ -0,0 +1,16 @@
+public class TreePreorderTraversal 
+{
+	public static void preOrder(Node root) {
+		if (root != null) {
+			System.out.printf("%d ", root.data);
+			preOrder(root.left);
+			preOrder(root.right);
+		}
+    }
+}
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+}
